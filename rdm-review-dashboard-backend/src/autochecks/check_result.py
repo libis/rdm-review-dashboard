e5bda142ba5adf64e7318796a7aac85d854d69d2
okaradeniz <oguzozgurkaradeniz@gmail.com>
2026-02-12 10:46:49 +0100

    added message parameter to autocheck results

--- rdm-review-dashboard-backend/src/autochecks/check_result.py
+++ rdm-review-dashboard-backend/src/autochecks/check_result.py
@@ -6,13 +6,15 @@ class CheckResult(BaseModel):
     result: bool | None = None
     last_checked: datetime | None = None
     warning: str | None = None
+    message: str | None = None
     additional_info: dict | None = None
 
-    def __init__(self, check_result:bool|None, warning:str|None, additional_info: dict|None=None, check_datetime=None):
+    def __init__(self, check_result:bool|None, warning:str|None, message: str|None=None, additional_info: dict|None=None, check_datetime=None):
         super().__init__()
         self.result = check_result
         self.last_checked = check_datetime or datetime.now()
         self.warning = warning 
+        self.message = message
         self.additional_info = additional_info or None 
         if self.result == True and self.warning is not None:
             raise ValueError("Warning must be empty when result is True.")
